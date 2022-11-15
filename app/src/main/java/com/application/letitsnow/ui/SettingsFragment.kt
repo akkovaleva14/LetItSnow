a1fbb337f3e428d5fe95c4748db8d66614c60398
aleksandra <akkovaleva14@gmail.com>
2022-11-16 00:27:39 +0300

    Fixed Spinner

--- app/src/main/java/com/application/letitsnow/ui/SettingsFragment.kt
+++ app/src/main/java/com/application/letitsnow/ui/SettingsFragment.kt
@@ -1,16 +1,22 @@
 package com.application.letitsnow.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.application.letitsnow.R
 import com.application.letitsnow.databinding.FragmentSettingsBinding
 
-class SettingsFragment : BaseFragment() {
+class SettingsFragment : BaseFragment(), AdapterView.OnItemSelectedListener {
 
     private var binding: FragmentSettingsBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +31,33 @@ class SettingsFragment : BaseFragment() {
         binding?.buttonArrowBack?.setOnClickListener {
             parentFragmentManager.popBackStack()
         }
+
+        // val spinner: Spinner = findViewById(R.id.spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding?.spinner?.adapter = adapter
+        }
+
+        binding?.spinner?.onItemSelectedListener = this
     }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
