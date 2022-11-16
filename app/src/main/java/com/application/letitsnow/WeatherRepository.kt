79d809326402631f9e3fdf69f05a7f135c7e3faf
aleksandra <akkovaleva14@gmail.com>
2022-11-17 01:15:15 +0300

    Repository

--- app/src/main/java/com/application/letitsnow/WeatherRepository.kt
+++ app/src/main/java/com/application/letitsnow/WeatherRepository.kt
@@ -1,8 +1,27 @@
 package com.application.letitsnow
 
+import com.application.letitsnow.data.Weather
 import com.application.letitsnow.network.ApiService
+import com.application.letitsnow.network.NetworkState
 
 class WeatherRepository (val api: ApiService) {
 
+
+    suspend fun getTemperatureOfTown(townName : String) : NetworkState<List<Weather>> {
+        val response = api.getTemperatureOfTown(town = townName)
+
+
+        return if (response.isSuccessful) {
+            val responseBody = response.body()
+            if (responseBody != null) {
+                NetworkState.Success(responseBody)
+            } else {
+                NetworkState.Error(response)
+            }
+        } else {
+            NetworkState.Error(response)
+        }
+
+    }
 
 }
